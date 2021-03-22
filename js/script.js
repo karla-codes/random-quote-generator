1408da9599e8db6ab5bc3eb26f9db1bbfb2914f0
karla-walker <indiablonix@gmail.com>
2021-03-21 21:55:01 -0500

    Added functions for generating random quote, printing quote, and updating background color. Added a 10 sec interval to printQuote function

--- js/script.js
+++ js/script.js
@@ -7,9 +7,7 @@ project 1 - A Random Quote Generator
 // Check the "Project Resources" section of the project instructions
 // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/***
- * `quotes` array
- ***/
+// array that holds at least 5 objects with quote info
 const quotes = [
   {
     quote:
@@ -66,13 +64,72 @@ const quotes = [
   },
 ];
 
-/***
- * `getRandomQuote` function
- ***/
+/**
+ * Generates a random number and returns a quote based on that number
+ *
+ * @param {arr} arr - Array of quotes
+ * @returns {object} The random quote
+ */
 
-/***
- * `printQuote` function
- ***/
+function getRandomQuote(arr) {
+  const number = Math.floor(Math.random() * arr.length);
+  const randomQuote = arr[number];
+  return randomQuote;
+}
+
+/**
+ * Generates random color
+ *
+ * @returns Random color and updates body background color
+ */
+
+function updateColor() {
+  const body = document.querySelector('body');
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+}
+
+/**
+ * Generates and prints out HTML string with random quote info
+ *
+ * @returns {string} Random quote + info
+ */
+
+function printQuote() {
+  const quote = getRandomQuote(quotes);
+  let html = `
+    <p class="quote">${quote.quote}</p>
+    <p class="source">${quote.source}
+  `;
+
+  if (quote.citation) {
+    html += `
+      <span class="citation">${quote.citation}</span>
+    `;
+  }
+
+  if (quote.year) {
+    html += `
+    <span class="year">${quote.year}</span>
+    `;
+  }
+
+  if (quote.tag) {
+    html += `
+    <span class="tag">${quote.tag}</span>
+    `;
+  }
+
+  html += `</p>`;
+
+  document.getElementById('quote-box').innerHTML = html;
+  updateColor();
+}
+
+// generates a new quote every 10 seconds
+window.setInterval(printQuote, 10000);
 
 /***
  * click event listener for the print quote button
